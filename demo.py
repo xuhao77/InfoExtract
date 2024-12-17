861d687d705448869b9401e4d17a50655b5944ba
xuhao77 <xuhao269634@163.com>
2024-12-17 21:50:52 +0800

    初始化项目

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from core.extract_helper import not_contains_chinese
 from core.task_config import TaskConfig
 from core.task import build_task
 
-
+# 待提取信息的类 你可以在类下方的文档中添加更多的字段说明 这将成为提示词的一部分
 class UserInfo(CheckMixin, Checked):
     """'hobby' summarized from what he often does"""
     user_name: str
@@ -27,6 +27,9 @@ task_config = TaskConfig(
     one_article_to_many_instance=True,  # 一篇文章可能有多个实例
     post_check_func=not_contains_chinese  # 针对大模型的回复的检查函数 只有通过才进行下一步解析
 )
+
+# TaskConfig中还可以指定数据库主键
+# 但要注意 一篇文章对应多个实例时 不可以将file_path作为主键 这会导致冲突
 
 if __name__ == '__main__':
     asyncio.run(build_task(task_config, UserInfo))
